78ee95a0eaf4dba0d5edc0a3d3e1e7215d4b1daf
IbrokhimN <161945489+IbrokhimN@users.noreply.github.com>
2026-07-24 23:42:34 +0500

    changes for pypi publication
    Ive published this library to pypi: https://pypi.org/project/pytinytensor/

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 import os
 import subprocess
 import pybind11
+from pathlib import Path
 from setuptools import setup, find_packages, Extension
 from setuptools.command.build_ext import build_ext
+
+long_description = Path(__file__).parent / "README.md"
+long_description = long_description.read_text(encoding="utf-8") if long_description.exists() else ""
 
 def check_cuda():
     try:
@@ -17,7 +21,6 @@ class CUDABuildExt(build_ext):
         original_compile = self.compiler._compile
 
         def custom_compile(obj, src, ext, cc_args, extra_postargs, pp_opts):
-            # extra_postargs может приходить как список или словарь — обрабатываем оба случая
             if isinstance(extra_postargs, dict):
                 gcc_postargs = extra_postargs.get('gcc', [])
                 nvcc_postargs = extra_postargs.get('nvcc', [])
@@ -26,12 +29,10 @@ class CUDABuildExt(build_ext):
                 nvcc_postargs = []
 
             if os.path.splitext(src)[1] == '.cu':
-                # nvcc собирает .cu файл
                 inc_flags = [f"-I{inc}" for inc in self.compiler.include_dirs]
                 cmd = ['nvcc', '-c', src, '-o', obj] + inc_flags + nvcc_postargs
                 self.spawn(cmd)
             else:
-                # g++ собирает .cpp файл (с pybind11)
                 original_compile(obj, src, ext, cc_args, gcc_postargs, pp_opts)
 
         self.compiler._compile = custom_compile
@@ -43,8 +44,8 @@ if check_cuda():
     cuda_module = Extension(
         'tinytensor.cuda_ops',
         sources=[
-            'tinytensor/backends/cuda_gpu.cu',       # Компилируется nvcc
-            'tinytensor/backends/cuda_binding.cpp'    # Компилируется g++
+            'tinytensor/backends/cuda_gpu.cu',       
+            'tinytensor/backends/cuda_binding.cpp'   
         ],
         include_dirs=[
             pybind11.get_include(),
@@ -59,14 +60,18 @@ if check_cuda():
         }
     )
     ext_modules.append(cuda_module)
-    print("🔥 CUDA найдена! Собираем cuBLAS бэкенд...")
+    print("CUDA найдена! Собираем cuBLAS бэкенд...")
 else:
-    print("⚠️ CUDA не найдена. Сборка продолжится на CPU (NumPy).")
+    print("CUDA не найдена. Сборка продолжится на CPU (NumPy).")
 
 setup(
-    name="tinytensor",
-    version="0.1.0",
+    name="pytinytensor",
+    version="0.1.1",
     description="мини ИИ фреймворк от IbrokimN ( github/IbrokhimN )",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="IbrokhimN",
+    url="https://github.com/IbrokhimN/tinytensor",
     packages=find_packages(include=["tinytensor", "tinytensor.*"]),
     ext_modules=ext_modules,
     cmdclass={'build_ext': CUDABuildExt},
